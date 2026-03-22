779ff3303f9bafc869edc9d02cf31bb3c49d7a2e
Nagy Attila (aha) <nagy.attis.81@gmail.com>
2026-03-22 23:48:31 +0100

    refactor: move files

--- src/icons.tsx
+++ src/icons.tsx
@@ -1,9 +1,9 @@
 import React from "react"
 
-import compactViewIcon from "./assets/icons/compact-view.svg"
-import detailedViewIcon from "./assets/icons/detailed-view.svg"
-import enFlagIcon from "./assets/icons/flag-en.svg"
-import huFlagIcon from "./assets/icons/flag-hu.svg"
+import compactViewIcon from "../assets/icons/compact-view.svg"
+import detailedViewIcon from "../assets/icons/detailed-view.svg"
+import enFlagIcon from "../assets/icons/flag-en.svg"
+import huFlagIcon from "../assets/icons/flag-hu.svg"
 
 export const CompactViewIcon = (): React.JSX.Element => <img src={compactViewIcon} width={28} height={18} alt="" aria-hidden style={{ display: "block" }} />
 
